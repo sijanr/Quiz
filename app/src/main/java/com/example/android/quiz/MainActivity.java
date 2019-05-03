cfb8bece6a43c8b46ad5e6cc8678d62ecbff03e3
sijanr <47872289+sijanr@users.noreply.github.com>
2019-05-02 21:21:02 -0700

    Replace the overflow button with menu icon

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -2,19 +2,15 @@ package com.example.android.quiz;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.CheckBox;
 
 import java.util.LinkedList;
 
@@ -37,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Correct: " + (quizAdapter.getTotalCorrect()) + "/" + (quizLists.size()) + "\nSelect reset from options to reset the game", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Correct: " + (quizAdapter.getTotalCorrect()) + "/" + (quizLists.size()) + "\nSelect reset button above to reset the game", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -81,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_rename) {
             Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
             finish();
